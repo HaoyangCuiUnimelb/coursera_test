834778c8ac78fdb22d7c7df23276da7a3fe2097e
HaoyangCuiUnimelb <haoyangcuiqe@gmail.com>
2019-07-24 22:04:34 +1000

    module 4 solution 2

--- module4_solution/SpeakGoodBye.js
+++ module4_solution/SpeakGoodBye.js
@@ -12,7 +12,7 @@
 
 (function (window) {
 	var byeSpeaker = {};
-	byeSpeaker.speakWord = "GoodBye";
+	byeSpeaker.speakWord = "Good Bye";
 	byeSpeaker.speak = function (name) {
   		console.log(byeSpeaker.speakWord + " " + name);
 	}
